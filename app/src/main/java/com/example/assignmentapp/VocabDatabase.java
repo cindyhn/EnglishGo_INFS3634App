e2381b03b78006ec9b740c2fd14763a25b6693cc
Cindy <z5117324@student.unsw.edu.au>
2019-04-26 18:12:22 +1000

    Editing changes

--- app/src/main/java/com/example/assignmentapp/VocabDatabase.java
+++ app/src/main/java/com/example/assignmentapp/VocabDatabase.java
@@ -4,7 +4,7 @@ public class VocabDatabase {
 
     public static String[] questions = {
             "What is the plural of fish?",
-            "What is adverb of quick",
+            "What is adverb of quick?",
             "Commonly known as a describing word, what adds detail or modifies a noun?",
             "What is the adverb of immediate?",
             "What is the opposite of adequate?"
